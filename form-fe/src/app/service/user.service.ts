5e0e4f2c991629e53c2106a1508393a243c84c09
Ankush Jha <jhaankush579@gmail.com>
2026-07-19 18:09:06 +0530

    Add environments file

--- form-fe/src/app/service/user.service.ts
+++ form-fe/src/app/service/user.service.ts
@@ -1,16 +1,17 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { environment } from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root',
 })
 export class User {
   constructor(private http: HttpClient){}
-  baseUrl = `http://localhost:3000/user`
+  baseUrl = environment.apiUrl
 
   postUserData(payload:any){
     console.log(payload)
-    return this.http.post(this.baseUrl , payload).subscribe(
+    return this.http.post(`${this.baseUrl}/user` , payload).subscribe(
       (res) => {console.log(res)},
       (error) => {console.log(error)}
     )
---
+++ form-fe/src/environments/environment.prod.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: true,
+  apiUrl: 'https://modern-form.onrender.com'
+};
